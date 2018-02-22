f26d916eb3b77f03683bac7cc219863c6d9a48d1
Karen Gyuzalyan <k.gyuzalyan@apollobytes.com>
2018-02-22 11:06:10 +0400

    (To-Do-App): Set to-do-app tasks-wrapper and title with css styles

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// @flow
+import React, {Component} from 'react';
+
+import _ from 'lodash';
+import TextField from 'material-ui/TextField';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="app-wrapper">
+                    <div className="tasks-wrapper">
+                        <div className="app-header">
+                            <h1 className="app-title">To Do App</h1>
+                            <div className="new-task display-flex">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
